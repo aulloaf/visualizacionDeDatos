8a4f6e1b42bb3a2d80d3aa3cf964ff49ba6cbd84
aulloaf <arturoulloa@udec.cl>
2026-04-15 12:51:12 -0400

    Change app.run_server to app.run

--- app.py
+++ app.py
@@ -451,4 +451,4 @@ def actualizar(a,b,c,d):
 # ==========================================================
 port = int(os.environ.get("PORT", 8050))
 
-app.run_server(host="0.0.0.0", port=port)
+app.run(host="0.0.0.0", port=port)
